1ad5a4bafc7f646f07e1a718272fa2a428680905
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-28 21:32:39 +0500

    Do some basic changes and make style and static data.Along side install the packages that are needed.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Signup from "./pages/auth/Signup";
-import PrivateRoutes from "./routes/PrivateRoutes";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import ManageTasks from "./pages/admin/ManageTasks";
-import CreateTask from "./pages/admin/CreateTask";
-import ManageUsers from "./pages/admin/ManageUsers";
-import UserDashboard from "./pages/user/UserDashboard";
-import MyTasks from "./pages/user/MyTasks";
-import TaskDetails from "./pages/user/TaskDetails";
+import { LoginPage, SignUpPage } from "./routes/allPagesLocalRoutes.js";
 
 function App() {
   return (
     <>
-      <Router>
+     <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<Signup />} />
-
-          {/* //Admin Routes */}
-          <Routes element={<PrivateRoutes allowedRoles={["admin"]} />}>
-            <Route path="/admin/dashboard" element={<AdminDashboard />} />
-            <Route path="/admin/tasks" element={<ManageTasks />} />
-            <Route path="/admin/create-task" element={<CreateTask />} />
-            <Route path="/admin/users" element={<ManageUsers />} />
-
-            {/* //User Routes */}
-            <Routes element={<PrivateRoutes allowedRoles={["admin"]} />}>
-              <Route path="/user/dashboard" element={<UserDashboard />} />
-              <Route path="/user/tasks" element={<MyTasks />} />
-              <Route path="/user/task-details:id" element={<TaskDetails />} />
-            </Routes>
-          </Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
         </Routes>
-      </Router>
-    </>
+      </Router> 
+       </>
   );
 }
 
---
+++ frontend/src/routes/allPagesLocalRoutes.js
@@ -0,0 +1,9 @@
+import LoginPage from "../pages/auth/LoginPage.jsx"
+import SignUpPage from "../pages/auth/SignUpPage.jsx"
+
+
+export {
+    LoginPage,
+    SignUpPage
+
+}
