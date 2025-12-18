fe21af135b246788b7e195c86a2c9adc6cfb2241
Awais12xd <lucifer.awaisxx213@example.com>
2025-12-18 16:27:16 +0500

    fix: Improve formatting and add secure cookie options in sendToken function

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,17 +1,21 @@
 import { apiResponse } from "./apiResponse.js";
 
-const sendToken = (user,statusCode,res) => {
-    try {
-        const token = user.getJwtToken();
+const sendToken = (user, statusCode, res) => {
+  try {
+    const token = user.getJwtToken();
     const options = {
-        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-    }
-    res.status(statusCode).cookie('token', token, options)
-    .json(new apiResponse(201, 'token sent successfully',user,token))
-    } catch (error) {
-        console.log("error while sending jwt" , error.message)
-    }
-}
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: true, // send only over HTTPS
+      sameSite: "none", // allow cross-site cookie
+    };
+    res
+      .status(statusCode)
+      .cookie("token", token, options)
+      .json(new apiResponse(201, "token sent successfully", user, token));
+  } catch (error) {
+    console.log("error while sending jwt", error.message);
+  }
+};
 
-export {sendToken}
+export { sendToken };
