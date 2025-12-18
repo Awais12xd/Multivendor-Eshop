4e600398c2de1bf743544971e2884d8313bdf886
Awais12xd <lucifer.awaisxx213@example.com>
2025-12-18 15:23:57 +0500

    Trying to fix cors issue

--- backend/app.js
+++ backend/app.js
@@ -29,8 +29,8 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
 const corsOptions = {
     origin: "http://localhost:5173" || "*",
     credentials: true,
-    // methods:["GET","PUT","DELETE","UPDATE"],
-    // allowHeaders:[" Content-Type","Authorization"]
+    methods:["GET","PUT","DELETE","UPDATE"],
+    allowHeaders:[" Content-Type","Authorization"]
 }
 
 //Middlewears
